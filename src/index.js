08242b46a831e1d7b9b9eb40b3924d68ad5b2fb8
ekeng <82917310+ekeng4488@users.noreply.github.com>
2022-06-16 18:55:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
 import './tailwind.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import * as serviceWorker from './serviceWorker'; 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  
   document.getElementById('root')
+
 );
 
 // If you want your app to work offline and load faster, you can change
